641a1c73e84a1fad0bea9e1303849b21fcfd7a1d
FireflySMCS2020 <38662108+FireflySMCS2020@users.noreply.github.com>
2018-04-24 11:07:01 -0400

    Add files via upload
    Finished automated emails

--- EmailTest.java
+++ EmailTest.java
@@ -8,14 +8,16 @@ import javax.activation.*;
 public class EmailTest {
 
 	public static void main(String[] args) {
-		Properties prop = System.getProperties();
-		prop.setProperty("mail.smtp.host", "localhost");
+		Properties prop = new Properties();
+		prop.setProperty("mail.smtp.host", "smtp.gmail.com");
+		prop.put("mail.smtp.starttls.enable", "true");
+		prop.put("mail.smtp.port", "587");
 		prop.put("mail.smtp.auth", true);
 		
-		Session s = Session.getInstance(prop, 
+		Session s = Session.getDefaultInstance(prop, 
 				new javax.mail.Authenticator() {  								//password authentication
 				protected PasswordAuthentication getPasswordAuthentication() {  
-					return new PasswordAuthentication("fireflySMCS2020@gmail.com","lightningbug");  
+					return new PasswordAuthentication("fireflySMCS2020@gmail.com", "lightningbug");  
 				}  
 		});
 		MimeMessage msg = new MimeMessage(s);
@@ -24,14 +26,18 @@ public class EmailTest {
 				msg.setFrom(new InternetAddress("fireflySMCS2020@gmail.com"));
 				msg.setSubject("Test Email");
 				msg.setText("This is a test text email");
+				System.out.println("sending email");
+				long start = System.nanoTime();
 				Transport.send(msg);
-				System.out.println("Successfully sent message");
+				System.out.printf("Message sent in %f seconds.", (System.nanoTime() - start)/1000000000.0);
 		}
 		catch(AddressException e){
 			System.out.println("Invalid email address");
+			e.printStackTrace();
 		}
 		catch(MessagingException e){
 			System.out.println("Messaging error");
+			e.printStackTrace();
 		}
 		
 	}
