712760f00ee286f3ef3b64342bd8edba458b7608
FireflySMCS2020 <38662108+FireflySMCS2020@users.noreply.github.com>
2018-04-25 08:28:48 -0400

    Add files via upload

--- EmailTest.java
+++ EmailTest.java
@@ -1,5 +1,8 @@
 package source;
 
+//FireflySMCS2020 Andrew Zhong 4/25/18 
+//Test email program, fill in recipients and senders
+
 import java.util.*;
 import javax.mail.*;
 import javax.mail.internet.*;
@@ -9,21 +12,21 @@ public class EmailTest {
 
 	public static void main(String[] args) {
 		Properties prop = new Properties();
-		prop.setProperty("mail.smtp.host", "smtp.gmail.com");
+		prop.setProperty("mail.smtp.host", "smtp.gmail.com");	//change accordingly with email service
 		prop.put("mail.smtp.starttls.enable", "true");
 		prop.put("mail.smtp.port", "587");
 		prop.put("mail.smtp.auth", true);
 		
 		Session s = Session.getDefaultInstance(prop, 
-				new javax.mail.Authenticator() {  								//password authentication
+				new javax.mail.Authenticator() {  			//password authentication
 				protected PasswordAuthentication getPasswordAuthentication() {  
-					return new PasswordAuthentication("fireflySMCS2020@gmail.com", "lightningbug");  
+					return new PasswordAuthentication("sender@email.com", "password");  
 				}  
 		});
 		MimeMessage msg = new MimeMessage(s);
 		try{
-				msg.addRecipient(Message.RecipientType.TO, new InternetAddress("fireflySMCS2020@gmail.com"));
-				msg.setFrom(new InternetAddress("fireflySMCS2020@gmail.com"));
+				msg.addRecipient(Message.RecipientType.TO, new InternetAddress("recipient@email.com"));
+				msg.setFrom(new InternetAddress("sender@email.com"));
 				msg.setSubject("Test Email");
 				msg.setText("This is a test text email");
 				System.out.println("sending email");
