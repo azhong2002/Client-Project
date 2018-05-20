e585b22f1fb8d8f1a0e40a3f1a9466c112ce6921
azhong2002 <35146067+azhong2002@users.noreply.github.com>
2018-05-19 21:46:19 -0400

    Created GUI class
    began GUi

---
+++ src/apfly/GUI.java
@@ -0,0 +1,22 @@
+package apfly;
+
+import javax.swing.*;
+import java.awt.BorderLayout;
+import java.awt.event.*;
+
+public class GUI extends JPanel implements ActionListener{
+
+	public GUI() {
+		
+	}
+	
+	public void actionPerformed(ActionEvent arg0) {
+		
+	}
+	
+	public static void main(String[] args) {
+		
+
+	}
+
+}
