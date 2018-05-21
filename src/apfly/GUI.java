70ea93658f017fa08f52cf68788c87bfd9109bac
FireflySMCS2020 <38662108+FireflySMCS2020@users.noreply.github.com>
2018-05-21 14:31:05 -0400

    Add files via upload
    Made Panels

--- src/apfly/GUI.java
+++ src/apfly/GUI.java
@@ -1,12 +1,24 @@
 package apfly;
 
 import javax.swing.*;
+
 import java.awt.BorderLayout;
+import java.awt.Panel;
 import java.awt.event.*;
+import java.applet.Applet;
 
 public class GUI extends JPanel implements ActionListener{
-
+	
+	private Panel north = new Panel();
+	private Panel south = new Panel();
+	
 	public GUI() {
+		
+		setLayout(new BorderLayout());
+		add(north, BorderLayout.NORTH);
+		add(south, BorderLayout.SOUTH);
+		north.setBackground();
+		
 		
 	}
 	
@@ -15,7 +27,12 @@ public class GUI extends JPanel implements ActionListener{
 	}
 	
 	public static void main(String[] args) {
-		
+		JFrame fram = new JFrame();
+		fram.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		GUI gui = new GUI();
+
+		fram.add(gui);
+		fram.setVisible(true);
 
 	}
 
