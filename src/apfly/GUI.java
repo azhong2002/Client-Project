d6a8a5837c8472892b2330799ef3b8bb541e33ef
azhong2002 <35146067+azhong2002@users.noreply.github.com>
2018-05-23 23:13:32 -0400

    Implemented Filechoosing, password protection, began TeacherPanel

--- src/apfly/GUI.java
+++ src/apfly/GUI.java
@@ -1,13 +1,18 @@
 package apfly;
 
+//Andrew Zhong 5/23/18 APFLy GUI
+//TODO : implement noTeacher email customization, FileUpload, send method
+
 import javax.swing.*;
+
+import tests.EmailTest;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Dimension;
+import java.awt.GridLayout;
 import java.awt.Panel;
 import java.awt.event.*;
-import java.applet.Applet;
+import java.util.ArrayList;
 
 public class GUI extends JFrame implements ActionListener, FocusListener{
 	
@@ -26,9 +31,13 @@ public class GUI extends JFrame implements ActionListener, FocusListener{
 	
 	//FILE UPLOAD
 	private Panel fileUpload = new Panel();
+	private JButton chooseFileBtn = new JButton("Upload a file");
+	private JFileChooser fc = new JFileChooser();
 	
 	//TEACHER LIST
-	private JScrollPane TeacherList = new JScrollPane();
+	private ArrayList<Teacher> teachList = new ArrayList<Teacher>();
+	private JPanel teacherPanelHolder = new JPanel();
+	private JScrollPane teacherViewPane = new JScrollPane(teacherPanelHolder);
 	
 	//MESSAGES
 	private JTextArea messages = new JTextArea("Messages: \n", 15, 30);
@@ -36,8 +45,8 @@ public class GUI extends JFrame implements ActionListener, FocusListener{
 	
 	public GUI(String title) {
 		super(title);
-		this.setSize(600,600);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(600,600);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		setLayout(new BorderLayout());	//adding all sections
 		add(north, BorderLayout.NORTH);
@@ -55,21 +64,34 @@ public class GUI extends JFrame implements ActionListener, FocusListener{
 		loginEntries.add(passIn, BorderLayout.SOUTH);
 		passIn.setForeground(Color.gray);
 		passIn.addFocusListener(this);
+		passIn.setEchoChar((char) 0);
 		
 		login.add(loginBtn, BorderLayout.EAST);	//button to log in
 		loginBtn.addActionListener(this);
 		
 		//FILEUPLOAD
 		north.add(fileUpload, BorderLayout.EAST);
+		fileUpload.add(chooseFileBtn, BorderLayout.NORTH);
+		chooseFileBtn.addActionListener(this);
 		
 		//TEACHERLILST
-		SE.add(TeacherList, BorderLayout.NORTH);
+		SE.add(teacherViewPane, BorderLayout.NORTH);
+		teacherPanelHolder.setLayout(new GridLayout());
 		
 		//MESSAGES
 		south.add(msgPanel, BorderLayout.WEST);	//Message panel
 		messages.setEditable(false);
 		//messages.setPreferredSize(new Dimension(300,300));
 		msgPanel.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		
+		this.pack();
+	}
+	
+	public void setTeachers(ArrayList<Teacher> teachList) {	//sets teacherpanels when new file is added
+		for (Teacher t: teachList) {
+			teacherPanelHolder.add(new TeacherPanel(t));
+		}
+		this.pack();
 	}
 	
 	public void display(String msg) {	//displays to messageboard
@@ -80,10 +102,11 @@ public class GUI extends JFrame implements ActionListener, FocusListener{
 		Object src = e.getSource();
 		if(src == loginBtn) {//sets login info if provided
 			if(userIn.getForeground() != Color.gray && passIn.getForeground() != Color.gray){
-				user = userIn.getText();	
-				pass = passIn.getPassword();
+				user = userIn.getText();
+				pass = new String(passIn.getPassword());
 				passIn.setText("Password");			//resets entry hints
 	        	passIn.setForeground(Color.gray);
+	        	passIn.setEchoChar((char) 0);
 	        	userIn.setText("Username");
 	        	userIn.setForeground(Color.gray);
 	        	display("Login credentials for " + user + " set.");
@@ -91,7 +114,29 @@ public class GUI extends JFrame implements ActionListener, FocusListener{
 			else{
 				display("Please provide a username and password.");
 			}
-		}	
+		}
+		else if(src == chooseFileBtn) {	//choosing file
+			String file;
+			int returnVal = fc.showOpenDialog(this);
+			if (returnVal == JFileChooser.APPROVE_OPTION) {
+                file = fc.getSelectedFile().getPath();
+                for(Teacher inp : DataReader.readData(file)) {	//add all new entries for each teacher into list
+                	boolean isNew = true;
+                	for(Teacher t: teachList) {
+                		if (t.name.equals(inp.name)) {
+                			t.addAll(inp.examList);
+                			isNew = false;
+                		}
+                	}
+                	if (isNew) {
+                		teachList.add(inp);
+                	}
+                }
+                setTeachers(teachList);
+                display("Added " + file + " to contents.");
+			}
+			
+		}
 	}
 	
 	public void focusGained(FocusEvent e) {
@@ -103,6 +148,7 @@ public class GUI extends JFrame implements ActionListener, FocusListener{
 		if(src == passIn && passIn.getForeground() == Color.gray) {	//removes pass entry hint
 			passIn.setText("");
 			passIn.setForeground(Color.black);
+			passIn.setEchoChar('\u2022');	//unicode for • character, hides password
 		}
         
 	}
@@ -113,9 +159,10 @@ public class GUI extends JFrame implements ActionListener, FocusListener{
 			userIn.setText("Username");
         	userIn.setForeground(Color.gray);
 		}
-		if(src == passIn && passIn.getText().equals("")) {	//resets pass entry hint
+		if(src == passIn && (new String(passIn.getPassword())).equals("")) {	//resets pass entry hint
 			passIn.setText("Password");
         	passIn.setForeground(Color.gray);
+        	passIn.setEchoChar((char) 0);	//makes hint text appear as text not dots
 		}
 		
 	}
